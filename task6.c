9bc2c5c0c89b03af07531afca19d23e7893cad25
Shadowmaple <1142319190@qq.com>
2018-10-17 06:50:22 +0800

    task

---
+++ task6.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sw(int a[][], int s, int b[])
+{
+	for (int i=0; i < s; i++){
+		for (int j=0; j<b[s]; j++)
+			for (int k=j+1, h; k<b[s]; k++)
+				if (abs(a[i][j]) > abs(a[i][k])) {
+					h = a[i][j];
+					a[i][j] = a[i][k];
+					a[i][k] = h;
+				}
+	}
+
+}
+
+/*int abs(int x)
+{
+	if (x < 0) return -x;
+	else return x;
+}*/
+
+int main()
+{
+	int a[100][100], n, s=0, b[100];
+	int check;
+
+	do {
+		scanf("%d", &n);
+		if (n > 100 || n < 0) puts("ERROR!");
+		else {
+			for (; b[s] < n; ++b[s]) {
+				scanf("%d", &a[s][b[s]]);
+				for (int i=0; i < b[s]; i++)
+					if ( abs( a[s][i] ) == abs( a[s][b[s]] ) )
+						goto error;
+			}
+			if (n != 0) s++;
+		}
+		error: puts("ERROR!");
+	} while (n != 0);
+
+	sw(a, s, b);
+
+	for (int i=0; i < s; i++) {
+		for (int j=0; j < b[s]; j++)
+			printf("%d ", a[i][j]);
+		putchar('\n');
+	}
+
+	return 0;
+}

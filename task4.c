fb46113af4e2cc0b260a771dab978b82e17a7e25
Shadowmaple <1142319190@qq.com>
2018-10-16 10:41:29 +0800

    task

--- task4.c
+++ task4.c
@@ -3,23 +3,31 @@
 int gc(int a, int b)
 {
 	int x;
-	while (x != 0) {
-		
+
+	while (b != 0) {
 		x=a % b;
+		a = b;
+		b = x;
 	}
+
+	return a;
 }
 int main()
 {
-	int a,b,c,d;
+	int a, b, c, d;
+
 	do {	
 		puts("Please enter two postive integer:");
-		scanf("%d%d", &a,&b);
-		if (a<=0 || b<=0) puts("error!");
-		if (a==b) puts("MUST DIFFERENT!");
-	} while (a<=0 || b<=0 || a==b);
+		scanf("%d", &a);
+		scanf("%d", &b);
+		if (a <= 0 || b <= 0) puts("error!");
+		if (a == b) puts("MUST DIFFERENT!");
+	} while (a <= 0 || b <= 0 || a == b);
 	
-	if (a<b) {d=a;a=b;b=d;}
+	if (a < b) {d = a; a = b; b = d;}
 
 	c = gc(a,b);
+	printf("%d\n", c);
+
 	return 0;
 }

7ff57e5806f7ab32cf85fc9c0868fce8bfa11cb6
Shadowmaple <1142319190@qq.com>
2018-10-17 12:55:12 +0800

    task

--- task6.c
+++ task6.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void sw(int a[][], int s, int b[])
+void sw(int a[][100], int s, int b[])
 {
 	for (int i=0; i < s; i++){
-		for (int j=0; j<b[s]; j++)
-			for (int k=j+1, h; k<b[s]; k++)
-				if (abs(a[i][j]) > abs(a[i][k])) {
+		for (int j=0; j<b[i]; j++)
+			for (int k=j+1, h; k<b[i]; k++)
+				if (abs(a[i][j]) < abs(a[i][k])) {
 					h = a[i][j];
 					a[i][j] = a[i][k];
 					a[i][k] = h;
@@ -15,36 +15,33 @@ void sw(int a[][], int s, int b[])
 
 }
 
-/*int abs(int x)
-{
-	if (x < 0) return -x;
-	else return x;
-}*/
-
 int main()
 {
 	int a[100][100], n, s=0, b[100];
 	int check;
+	/*s为行数，组数，从0开始；b[]为各组的元素个数*/
 
 	do {
 		scanf("%d", &n);
 		if (n > 100 || n < 0) puts("ERROR!");
 		else {
 			for (; b[s] < n; ++b[s]) {
-				scanf("%d", &a[s][b[s]]);
+				scanf("%d", &a[s][ b[s] ] );
+
 				for (int i=0; i < b[s]; i++)
-					if ( abs( a[s][i] ) == abs( a[s][b[s]] ) )
-						goto error;
+					if ( abs( a[s][i] ) == abs( a[s][ b[s] ] ) ) {
+						puts("error"); break;
+					}
 			}
 			if (n != 0) s++;
 		}
-		error: puts("ERROR!");
 	} while (n != 0);
 
 	sw(a, s, b);
+	putchar('\n');
 
 	for (int i=0; i < s; i++) {
-		for (int j=0; j < b[s]; j++)
+		for (int j=0; j < b[i]; j++)
 			printf("%d ", a[i][j]);
 		putchar('\n');
 	}

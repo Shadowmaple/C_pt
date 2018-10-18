870b117228688dde6502be27204a61e52d06a912
Shadowmaple <1142319190@qq.com>
2018-10-18 22:41:01 +0800

    task

---
+++ task8.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[100][100], b[100], n, line;
+	int check;
+
+	do {
+		sacnf("%d", &n);
+		if (n < 2 && n <= 100)
+		{
+			for () {
+
+			}
+		} else if (n != 0) {
+			puts("error!");
+		}
+	} while (n != 0);
+
+
+	return 0;
+}

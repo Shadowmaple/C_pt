e80738ba0f90211925bb39d8769abc753f03c4df
Shadowmaple <1142319190@qq.com>
2018-10-19 22:55:02 +0800

    task

--- task9.c
+++ task9.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+//返回2月份的天数
 int rn(int run)
 {
 	if (run == 'F') return 28;
 	else return 29;
 }
 
+//xy函数计算并返回天数
 int xy(int run, int b, int c)
 {
 	int r;
@@ -33,7 +35,9 @@ void num(int a[], int b[], int c[], int re[], int s)
 
 	for (int i=0; i < s; i++) {
 		int run = 'T';
-
+		//run为T则为闰年
+		
+		//判断是否为闰年
 		if (a[i] % 100) {
 			if (a[i] % 4) {
 				run = 'F';
@@ -55,10 +59,12 @@ void num(int a[], int b[], int c[], int re[], int s)
 }
 
 int main()
-{
+{	
+	//line为行数即有几组，re[]为最后的第几天
 	int year[100], month[100], day[100];
 	int re[100], line =0;
-
+	
+	//输入，输入0则停止
 	do {
 		scanf("%d/%d/%d", &year[line], &month[line], &day[line]);
 		if (month[line] > 12 || month[line] < 1 || day[line] > 31 ||
@@ -70,7 +76,8 @@ int main()
 	} while (year[line] != 0);
 
 	num(year, month, day, re, line);
-
+	
+	//输出
 	for (int i=0; i < line; i++)
 		printf("%d\n", re[i]);
 

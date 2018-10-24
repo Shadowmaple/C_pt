dad312664ae387725af8fb381e90079b7ca1b1e1
Shadowmaple <1142319190@qq.com>
2018-10-24 17:32:43 +0800

    test

---
+++ test1.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int n;
+int num[81];
+int a[9] = {0};
+
+void sort(int x)
+{
+	if (x == n) {
+		for (int j =0; j < n; j++)
+			printf("%d ", num[j]);
+		putchar('\n');
+	}
+
+	for (int i=0; i < n; i++) {
+		if (a[i] ==0) {
+			a[i] = 1;
+			num[x] = i+1;
+			sort(x+1);
+			a[i] = 0;
+		}
+	}
+}
+
+int main()
+{
+
+	scanf("%d", &n);
+
+	sort(0);
+
+	return 0;
+}

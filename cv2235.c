18d49f8c67b604548440c498b29e7e89b9dcc749
Shadowmaple <1142319190@qq.com>
2018-10-25 23:36:09 +0800

    codevs

--- cv2235.c
+++ cv2235.c
@@ -2,11 +2,18 @@
 
 int main ()
 {
-	int n
+	int n, y;
+	double m;
 
+	scanf("%d %lf", &n, &m);
+	int x;
+	x = m * n /10;
+	y = x % 10;
 
+	if (y <= 9 && y > 4)
+		x = x +10;
+	x = x - y;
 
-
-
+	printf("%d\n", x);
 	return 0;
 }

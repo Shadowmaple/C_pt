495faf7a8ef66c3b5ea8e6dcbb400bb685f896b3
Shadowmaple <1142319190@qq.com>
2018-10-25 14:16:54 +0800

    codevs

--- cv1205.c
+++ cv1205.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*int sh(char a[])
-{
-	int i=0;
-	while (a[i++]) ;
-	return i-1;
-
-}
-*/
-
 int main()
 {
 	char a[100], b[100][100];
@@ -17,31 +8,22 @@ int main()
 
 	scanf("%[^\n]", a);
 	n = strlen(a);
-	puts(a);
-	printf("%d\n", n);
 
 	int n1=0, n2, k=0;
-	for (int i=0; i < n; i++) {
-		if (a[i] == ' ') {
+	for (int i=0; i <= n; i++) {
+		if (a[i] == ' ' || a[i] == '\0') {
 			n2 = i-1;
-			putchar('+');
 			for (int j =n1, h=0; j <= n2; h++,j++) {
 				b[k][h] = a[j];
-				putchar(b[k][h]);
-				putchar('*');
-			
 			}
 			k++;
-			n1 = i +1;	
+			n1 = i +1;
 		}
-	
-	
-	
 	}
 	
-	for (int i= k-1; i >= 0; i--) {
-		puts(b[i]);
-	}
+	for (int i= k-1; i >= 0; i--)
+		printf("%s ", b[i]);
 
+	putchar('\n');
 	return 0;
 }

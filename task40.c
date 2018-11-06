25530d027f26e286ef00c0ace0ab13d2fed37dd6
Shadowmaple <1142319190@qq.com>
2018-11-06 09:01:54 +0800

    task

--- task40.c
+++ task40.c
@@ -4,36 +4,34 @@ int main()
 {
 	int T, K;
 	scanf("%d%d", &T, &K);
-	int n1[K], b[K], n2[K], t[K], x;
+	int n1, b, n2, t, x;
 	x = T;
 	for (int i=0; i < K; i++) {
-		scanf("%d%d%d%d", &n1[i], &b[i], &t[i], &n2[i]);
-	}
+		scanf("%d%d%d%d", &n1, &b, &t, &n2);
 
-	for (int i=0; i < K; i++) {
-		if (t[i] > x) {
+		if (t > x) {
 			printf("Not enough tokens.  Total = %d.\n", x);
 			continue;
 		}
 
-		if (n2[i] > n1[i]) {
-			if (b[i]) {
-				x +=t[i];
-				printf("Win %d!  Total = %d.\n", t[i], x);
+		if (n2 > n1) {
+			if (b) {
+				x +=t;
+				printf("Win %d!  Total = %d.\n", t, x);
 			}
 			else{
-				x -=t[i];
-				printf("Lost %d.  Total = %d.\n", t[i], x);
+				x -=t;
+				printf("Lose %d.  Total = %d.\n", t, x);
 			}
 		}
 		else {
-			if (b[i]) {
-				x -=t[i];
-				printf("Lost %d.  Total = %d.\n", t[i], x);
+			if (b) {
+				x -=t;
+				printf("Lose %d.  Total = %d.\n", t, x);
 			}
 			else {
-				x +=t[i];
-				printf("Win %d!  Total = %d.\n", t[i], x);
+				x +=t;
+				printf("Win %d!  Total = %d.\n", t, x);
 			}
 		}
 		if (!x){

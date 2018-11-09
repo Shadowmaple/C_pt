e8c5796dc47abd0d6e4d838f30a03b200c6350d5
Shadowmaple <1142319190@qq.com>
2018-11-09 18:01:33 +0800

    task

--- task21.c
+++ task21.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
+int count(int n, int x)
+{
+	if (n == 1) return 1;
+	int sum =0;
+	for (int i=0; i < 3; i++) {
+		if (i == x) continue;
+		if (n == 2 && i == 0) continue;
+		int y = n-1;
+		sum += count(y, i);
+	}
+	return sum;
+}
+
 int main()
 {
 	int n;
+	int x = 0, result;
 	while (1) {
 		scanf("%d", &n);
 		if (!n) break;
-		int
-	
+		result = count(n, x);
+		printf("%d\n", result % 10000);
 	}
-
 
 	return 0;
 }

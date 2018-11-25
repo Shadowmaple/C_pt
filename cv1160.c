ed6c4e3ff6a0a69356db7324a839e3799f20cbf4
Shadowmaple <1142319190@qq.com>
2018-11-25 16:34:50 +0800

    task

---
+++ cv1160.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	int n, count = 1;
+	scanf("%d", &n);
+	int a[n][n];
+	int i,j;
+	i = j = n/2;
+	for (int m=0; m<n/2; m++) {
+		
+	
+	}
+
+
+
+
+
+
+
+	for (int i=0; i<n; i++) {
+		for (int j=0; j<n; j++) {
+			printf("%d ", a[i][j]);
+		}
+		putchar('\n');
+	}
+	return 0;
+}

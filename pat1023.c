82b4f47cff465439788d52da42e51308c30c0113
Shadowmaple <1142319190@qq.com>
2019-01-18 18:46:35 +0800

    PAT

--- pat1023.c
+++ pat1023.c
@@ -5,21 +5,20 @@ int main()
 	int n[10];
 	for(int i=0; i < 10; i++)
 		scanf("%d", &n[i]);
-	char num[51];
 	int count=0;
 	if (n[0])
 		for (int i=1; i < 10; i++)
-			if (n[i]--) {
-				num[0] = i+'0';
+			if (n[i]) {
+				printf("%d", i);
 				count++;
+				n[i]--;
 				break;
 			}
+
 	for (int i=0; i < 10; i++)
 		while (n[i]--)
-			num[count++] = i+'0';
+			printf("%d", i);
 
-	for (int i=0; i < count; i++)
-		printf("%c", num[i]);
 	putchar('\n');
 	return 0;
 }

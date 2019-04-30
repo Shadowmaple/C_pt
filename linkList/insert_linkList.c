9af7f8e8f4994fa43c30175d7654983097ab87f6
Shadowmaple <1142319190@qq.com>
2019-04-30 08:31:01 +0800

    modify

--- linkList/insert_linkList.c
+++ linkList/insert_linkList.c
@@ -8,6 +8,16 @@ struct linkList
 };
 typedef struct linkList NODE;
 
+void show(NODE *head)
+{   
+    NODE *p = head->next;
+    while (p != NULL) {
+        printf("%d ", p->number);
+        p = p->next;
+    }
+    putchar('\n');
+}
+
 int main()
 {
     NODE *head, *tail, *new;
@@ -17,7 +27,7 @@ int main()
     head->next = NULL;
     tail = head;
 
-    puts("输入一组原始数字：")
+    puts("输入一组原始数字：");
     while(1) {
         scanf("%d", &number);
         if (number == -1)
@@ -30,22 +40,33 @@ int main()
         tail->next = new;
         tail = new;
     }
+    //显示当前数据
+    show(head);
 
     int position;
-    puts("输入要插入的位置和数字：")
-    while (scanf("%d", &position) != -1) {    
+    puts("输入要插入的位置和数字(-1为停止)：");
+    while (1) {
+        scanf("%d", &position);
+        if (position == -1)
+            break;
         scanf("%d", &number);
 
-        NODE *p;
+        NODE *p = head;
         for (int i=0; i<=position && p != NULL; i++)
             p = p->next;
-        if (p == NULL)
-            puts("该节点不存在")
+        if (p == NULL) {
+            puts("该节点不存在");
+            continue;
+        }
         
+        new = (NODE *) malloc (sizeof(NODE));
+        new->next = p->next;
+        new->number = number;
+        p->next = new;
 
+        //展示插入后的结果
+        show(head);
     }
-
-
 
     return 0;
 }

00b4faecf2483f56c62b2afb01fe8cb2be52ab3f
Shadowmaple <1142319190@qq.com>
2019-05-09 00:25:29 +0800

    new

---
+++ PAT/1021.c
@@ -0,0 +1,34 @@
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+
+void clear_blank(char *num)
+{
+    int i=0;
+    while (num[i] == ' ')
+        i++;
+    
+    strcpy(num, num + i);
+}
+
+int main()
+{
+    int A, B, D;
+    scanf("%d%d%d", &A, &B, &D);
+
+    long sum = A + B;
+    char num[1001];
+    memset(num, ' ', 1000);
+
+    int i = 1000;
+    while (sum > 0) {
+        num[i--] = sum % D + '0';
+        sum /= D;
+        //printf("_%c\n", num[i+1]);
+    }
+    num[1001] = '\0';
+    clear_blank(num);
+
+    printf("%s\n", num);
+    return 0;
+}

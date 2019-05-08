80715b9e3d327e46d5454292da63c39e6cbdb645
Shadowmaple <1142319190@qq.com>
2019-05-08 23:43:47 +0800

    modify

--- PAT/1020.c
+++ PAT/1020.c
@@ -8,10 +8,10 @@ struct cake
     float unit_price; //月饼的单价
 } mooncake[1000];
 
-void compare(const void *a, const void *b)
+int compare(const void *a, const void *b)
 {
-    struct cake price1 = *(float *) a;
-    struct cake price2 = *(float *) b;
+    struct cake price1 = *(struct cake *) a;
+    struct cake price2 = *(struct cake *) b;
     return price1.unit_price < price2.unit_price;
     //降序
 }
@@ -24,23 +24,25 @@ int main()
     
     //输入库存量
     for (int i=0; i < kind; i++)
-        scanf("%d", mooncake[i].storage);
+        scanf("%d", &mooncake[i].storage);
     //输入总售价，并计算单价
     for (int i=0; i < kind; i++) {
-        scanf("%d", mooncake[i].price);
+        scanf("%f", &mooncake[i].price);
         mooncake[i].unit_price = mooncake[i].price / mooncake[i].storage;
     }
     qsort(mooncake, kind, sizeof(mooncake[0]), compare);
 
     float total = 0;
-    for (int i=0; ; i++) {
+    for (int i=0; i < kind; i++) {
         if (mooncake[i].storage >= need) {
-            total = need * mooncake[i].unit_price;
+            total += need * mooncake[i].unit_price;
             break;
         }
+        total += mooncake[i].storage * mooncake[i].unit_price;
         need -= mooncake[i].storage;
-        
+
     }
+
     printf("%.2f\n", total);
     return 0;
 }

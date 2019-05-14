ab767bb081df8aa12c409e24c55824ab903802cd
Shadowmaple <1142319190@qq.com>
2019-05-14 22:17:59 +0800

    modify

--- PAT/1025.c
+++ PAT/1025.c
@@ -1,53 +1,55 @@
 # include <stdio.h>
 
-struct link
-{
-    int data;
-    struct link *next;
-    int num;
-};
-
-struct data
+struct linkList
 {
     int address;
     int data;
-    int next_address;
+    int next;
 };
 
 int main()
 {
     int N, K, address;
-    //char address[6];
     scanf("%d%d%d", &address, &N, &K);
-    struct link *head, *tail, *new;
-    struct data number[N];
-
-    head = (struct link *)malloc (sizeof(struct link));
-    head->next = address;
-    tail = head;
+    struct linkList link_x[N];
+    struct linkList *link[N];
 
     for (int i=0; i < N; i++) {
-        scanf("%d%d%d", &number[i].address, &number[i].data, &number[i].next_address);
+        link[i] = link_x + i;
+        //puts("---");
+        scanf("%d%d%d", &link[i]->address, &link[i]->data, &link[i]->next);
     }
 
-    for (int j=0; j < N; j++) {
-        for (int i=0; i < N; i++) {
-            if (tail->next != number[i].address)
+    //链接链表，即对数组排序
+    for (int i=0; i < N-1; i++) {
+        for (int j=i; j < N; j++) {
+            if (link[j]->address != (i ? link[i-1]->next : address))
                 continue;
+            //swap(link[i], link[j]);
+            struct linkList *a = link[i];
+            link[i] = link[j];
+            link[j] = a;
+            break;
+        }
+        //printf("---%05d %d %05d\n", link[i]->address, link[i]->data, link[i]->next);
+    }
+    //printf("---%05d %d -1\n", link[N-1]->address, link[N-1]->data);
 
-            new = number[i].address;
-            new->data = number[i].data;
-            if (number[i].next_address == -1)
-                new->next = NULL;
-            new->next = number[i].next_address;
-
-            tail->next = number[i].address;
-            tail = new;
+    //反转链表，对数组交换顺序
+    for (int i=0; i < N / K; i++) {
+        for (int j = i*K, m = j+K-1; j < K / 2; j++, m--) {
+            //swap(link[j], link[m]);
+            struct linkList *a = link[j];
+            link[j] = link[m];
+            link[m] = a;
         }
     }
 
-    
-
-
+    //putchar('\n');
+    for (int i=0; i < N-1; i++) {
+        printf("%05d %d %05d\n", link[i]->address, link[i]->data, link[i+1]->address);
+        //注意最后输出的下一节点地址不是用本节点的next值输出
+    }
+    printf("%05d %d -1\n", link[N-1]->address, link[N-1]->data);
     return 0;
 }

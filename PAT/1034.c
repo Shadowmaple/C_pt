f5601b050b0a78af22fc7d026c22b17f081aa607
Shadowmaple <1142319190@qq.com>
2019-05-26 22:28:14 +0800

    modify

--- PAT/1034.c
+++ PAT/1034.c
@@ -24,6 +24,40 @@ void convert(int *a, int *b, int *c)
 {
     *c = *a / *b;
     *a %= *b;
+    //负号前置
+    if (*c != 0 && *a < 0) {
+        *a = -*a;
+     //   *c = -*c;
+    }
+}
+
+void pr_num(int a, int b, int c)
+{
+    convert(&a, &b, &c);
+    if (c < 0 || a < 0)
+        printf("(");
+    if (c)
+        printf("%d", c);
+    if (c && a)
+        putchar(' ');
+    if (a)
+        printf("%d/%d", a, b);
+    if (c < 0 || a < 0)
+        printf(")");
+    if (!c && !a)
+        printf("0");
+}
+
+void display(int a1, int b1, int a2, int b2, char sign)
+{
+    int c1 = 0, c2 = 0;
+    //输出等号左边
+    pr_num(a1, b1, c1);
+    printf(" %c ", sign);
+
+    //输出等号右边
+    pr_num(a2, b2, c2);
+    printf(" = ");
 }
 
 int main()
@@ -38,7 +72,53 @@ int main()
     int sign_2 = (a2 >= 0) ? 1 : 0;
     //公共的分母数
     int b_both = b1 * b2;
+    int a3, b3, c3;
 
+    //加
+    display(a1, b1, a2, b2, '+');
+    a3 = a1 * b2 + a2 * b1;
+    b3 = b_both;
+    c3 = 0;
+    simplify(&a3, &b3);
+    pr_num(a3, b3, c3);
+    putchar('\n');
+
+    //减
+    display(a1, b1, a2, b2, '-');
+    a3 = a1 * b2 - a2 * b1;
+    b3 = b_both;
+    c3 = 0;
+    simplify(&a3, &b3);
+    pr_num(a3, b3, c3);
+    putchar('\n');
+
+    //乘
+    display(a1, b1, a2, b2, '*');
+    a3 = a1 * a2;
+    b3 = b1 * b2;
+    c3 = 0;
+    simplify(&a3, &b3);
+    pr_num(a3, b3, c3);
+    putchar('\n');
+
+    //除
+    display(a1, b1, a2, b2, '/');
+    //除数为0
+    if (!a2) {
+        puts("Inf");
+        return 0;
+    }
+    a3 = a1 * b2;
+    b3 = b1 * a2;
+    //负号移到分子上
+    if (b3 < 0) {
+        a3 = -a3;
+        b3 = -b3;
+    }
+    c3 = 0;
+    simplify(&a3, &b3);
+    pr_num(a3, b3, c3);
+    putchar('\n');
 
     return 0;
 }

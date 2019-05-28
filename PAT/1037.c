679d10b343b9fa5006cdf800f2f4978815bfaddd
Shadowmaple <1142319190@qq.com>
2019-05-28 10:51:42 +0800

    new

---
+++ PAT/1037.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+int main()
+{
+    int galleon_p, sickle_p, knut_p;
+    int galleon_a, sickle_a, knut_a;
+    scanf("%d.%d.%d", &galleon_p, &sickle_p, &knut_p);
+    scanf("%d.%d.%d", &galleon_a, &sickle_a, &knut_a);
+
+    long sum_p = galleon_p * 17 * 29 + sickle_p * 29 + knut_p;
+    long sum_a = galleon_a * 17 * 29 + sickle_a * 29 + knut_a;
+    long minus = sum_a - sum_p;
+
+    int gallen_result, sickle_result, knut_result;
+    gallen_result = minus / (17 * 29);
+    sickle_result = minus % (17 * 29) / 29;
+    knut_result = minus % 29;
+
+    printf("%d.%d.%d/n", gallen_result, sickle_result, knut_result);
+
+    return 0;
+}

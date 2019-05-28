0c06866af5d2adf61aead67f5fb4fb9bef403a6a
Shadowmaple <1142319190@qq.com>
2019-05-28 11:34:28 +0800

    new

---
+++ PAT/1039.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include <string.h>
+
+int main()
+{
+    char have[1001], want[1001];
+    scanf("%s%s", have, want);
+    int len_have = strlen(have), len_want = strlen(want);
+    int isHave[len];
+    int get = 0;
+    memset(isHave, 1, sizeof(int));
+
+    for (int i=0; i < len_want; i++) {
+        for (int j=0; j < len_have; j++) {
+            if (want[i] != have[j] || !isHave[j])
+                continue;
+            isHave[j] = 0;
+            get++;
+            break;
+        }
+        if (get == len_want)    break;
+    }
+    if (get < len_want)
+        printf("No %d\n", len_want - get);
+    else
+        printf("Yes %d\n", len_have - len_want);
+
+    return 0;
+}

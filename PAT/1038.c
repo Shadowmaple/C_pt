ef9c6d648076d83221408580a33527397eb017d6
Shadowmaple <1142319190@qq.com>
2019-05-28 11:02:01 +0800

    new

---
+++ PAT/1038.c
@@ -0,0 +1,27 @@
+# include <stdio.h>
+
+int main()
+{
+    int N, K;
+    scanf("%d", &N);
+    int num[N];
+    for (int i=0; i < N; i++)
+        scanf("%d", num + i);
+    scanf("%d", &K);
+    int grade[K], total[K] = {0};
+    for (int i=0; i < K; i++)
+        scanf("%d", grade + i);
+
+    for (int i=0; i < N; i++) {
+        for (int j=0; j < K; j++) {
+            if (num[i] == grade[j]) {
+                total[j]++;
+                break;
+            }
+        }
+    }
+    for (int i=0; i < K; i++)
+        printf("%d%c", total[i], (i==K-1)? '\n':' ');
+    
+    return 0;
+}

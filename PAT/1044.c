9a17cfa1829093a7da8a7d49631e27325ac03eba
Shadowmaple <1142319190@qq.com>
2019-06-04 10:19:19 +0800

    modify

--- PAT/1044.c
+++ PAT/1044.c
@@ -7,9 +7,8 @@ int main()
     int N;
     char mars[13][5] = {"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
     char mars_high[12][4] = {"tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
-
-    scanf("%d\n", &N);
     char num[8];
+    scanf("%d\n", &N);
 
     for (int i=0; i < N; i++) {
         // scanf("%[^\n]", num);
@@ -22,7 +21,7 @@ int main()
         }
         num[j] = '\0';
 
-        //若是数字
+        //若是地球数字
         if (isdigit(num[0])) {
             int n = 0;
             for (int j=0; num[j] != '\0'; j++) {
@@ -33,28 +32,35 @@ int main()
             else if (n % 13)
                 printf("%s %s\n", mars_high[n / 13 - 1], mars[n % 13]);
             else
+                // 末尾的零不能保留，像英文里的一样，如20--twenty
                 printf("%s\n", mars_high[n / 13 - 1]);
         
         } else {
-            if (num[3] == '\0') {
-                for (int j=0; j < 12; j++) {
+            //若是火星文
+            //一个火星文的情况
+            if (num[3] == '\0' || num[3] == 't') {
+                for (int j=0; j < 13; j++) {
                     if (!strcmp(num, mars[j])) {
                         printf("%d\n", j);
                         break;
                     }
+                    // 单个进位火星文
                     if (!strcmp(num, mars_high[j])) {
                         printf("%d\n", (j + 1) * 13);
                         break;
                     }
                 }
             } else {
+                // 两个火星文的情况
                 int n = 0;
+                // 计算第一个火星文
                 for (int j=0; j < 12; j++) {
                     if (!strncmp(num, mars_high[j], 3)) {
                         n = j + 1;
                         break;
                     }
                 }
+                // 第二个火星文
                 for (int j=0; j < 13; j++) {
                     if (!strcmp(num + 4, mars[j])) {
                         n = n * 13 + j;

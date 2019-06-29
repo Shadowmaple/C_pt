55f651e6be99980d16eea5cb3dd97a4e4bdeeafa
Shadowmaple <1142319190@qq.com>
2019-06-29 09:19:16 +0800

    modify

--- PAT/1047.c
+++ PAT/1047.c
@@ -1,8 +1,26 @@
 # include <stdio.h>
+# include <string.h>
 
 int main()
 {
 	int n;
+	int id[n], grade[n];
+	int k = 0;
+
+	for (int i=0; i < n; i++) {
+		grade[i] = 0;	
+		id[i] = 0;
+	}
+	for (int i=0; i < n; i++) {
+		int id_buffer, grade_buffer, buffer;
+		scanf("%d-%d %d", &id_buffer, &buffer, &grade_buffer);
+		for (int j=0; j < k; j++) {
+			if (id[k] == id_buffer) {
+				grade[k] += grade_buffer;
+				break;
+			}
+		}
+	}
 
 	return 0;
 }

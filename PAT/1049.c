8d81f6ff4b41a844b4a0f7cd0b6a88bef3bf17cf
Shadowmaple <1142319190@qq.com>
2019-06-30 10:32:34 +0800

    modify

--- PAT/1049.c
+++ PAT/1049.c
@@ -4,17 +4,16 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    float num[n], sum = 0;
-
-    for (int i=0; i < n; i++)
-        scanf("%f", &num[i]);
+    float num, sum = 0;
 
     for (int i=1; i <= n; i++) {
+        scanf("%f", &num);
+
         if (i <= n / 2)
-            sum += num[i - 1] * (i * (n - i + 1));
+            sum += num * (i * (n - i + 1));
         else {
             int k = n - i + 1;
-            sum += num[i - 1] * (k * (n - k + 1));
+            sum += num * (k * (n - k + 1));
         }
     }
     printf("%.2f\n", sum);

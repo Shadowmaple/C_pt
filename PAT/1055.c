bc3e20518fbe9c1e6a04a112685e4dfcee16dd72
Shadowmaple <1142319190@qq.com>
2019-07-04 14:17:55 +0800

    modify

--- PAT/1055.c
+++ PAT/1055.c
@@ -13,12 +13,12 @@ int compar(const void *a, const void *b)
     pep *x = (pep *) a;
     pep *y = (pep *) b;
     if (x->height > y->height)
-        return 1;
+        return -1;  // 置换
     else if (x->height < y->height)
-        return -1;
-    else if (x->name > y->name)
         return 1;
-    else return -1;
+    else if (strcmp(x->name, y->name) < 0)
+        return -1;
+    else return 1;
 }
 
 int main()
@@ -35,38 +35,24 @@ int main()
 
     int line = N / K;   // 每排人数
     int line_max = N % K + line;    // 一排最多人数
-    char row[K][line_max][9];
     int index = 0;
-
-    for (int i = K - 1; i >= 0; i--) {
-        // 该排人数
-        int num = !i ? line_max : line ;
-        // 中间位置
-        int cen = num / 2 + 1;
-        // 该排的人
-        pep row_man[num];
-        for (int j = 0; j < num; j++)
-            row_man[j] = man[index++];
-
-        qsort(row_man, num, sizeof(pep), compar);
-
-        int j = num - 1;
-        strcpy(row[i][cen - 1], row_man[j--].name);
-        
-        int left = cen - 2, right = cen;
-        while (left >= 0 || right < num) {
-            if (left >= 0)
-                strcpy(row[i][left--], row_man[j--].name);
-            
-            if (right < num)
-                strcpy(row[i][right++], row_man[j--].name);
-        }
-    }
 
     for (int i = 0; i < K; i++) {
         int num = !i ? line_max : line;
+        char name[num][9];
+        int cen = num / 2 + 1;
+        int left = cen - 2, right = cen;
+        
+        strcpy(name[cen - 1], man[index++].name);
+        while (left >= 0 || right < num) {
+            if (left >= 0)
+                strcpy(name[left--], man[index++].name);
+            if (right < num)
+                strcpy(name[right++], man[index++].name);
+        }
+
         for (int j = 0; j < num; j++)
-            printf("%s%c", row[i][j], j == num - 1 ? '\n' : ' ');
+            printf("%s%c", name[j], j == num - 1 ? '\n': ' ');
     }
 
     return 0;

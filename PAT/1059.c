bd564dd908c4f7ce9cd446d4e3d3576bc44b4083
Shadowmaple <1142319190@qq.com>
2019-07-06 09:40:02 +0800

    modify

--- PAT/1059.c
+++ PAT/1059.c
@@ -16,6 +16,22 @@ int isPrime(int num)
     else return 0;
 }
 
+int compar(const void *a, const void *b)
+{
+
+}
+
+int search(char *target, char (*id)[5], int left, int right)
+{
+    int cen = (right + left) / 2;
+    if (!strcmp(target, id[cen])) return cen;
+    else if (left > right) return -1;
+    int x = search(target, id, left, cen - 1);
+    int y = search(target, id, cen + 1, right);
+    if (x != -1) return x;
+    if (y != -1) return y;
+}
+
 int main()
 {
     int N, K;
@@ -30,28 +46,25 @@ int main()
     scanf("%d", &K);
 
     char check[K][5];
+    qsort()
     for (int i=0; i < K; i++) {
         scanf("%s", check[i]);
-        // search(id, flag, check[i], N);
-        int f = 1;
-		for (int j=0; j < N; j++) {
-			if (!strcmp(id[j], check[i])) {
-				if (flag[j]) printf("%s: Checked\n", check[i]);
-				else if (!j) {
-					printf("%s: Mystery Award\n", check[i]);
-					flag[j] = 1;
-				} else if (isPrime(j + 1)) {
-					printf("%s: Minion\n", check[i]);
-					flag[j] = 1;
-				} else {
-					printf("%s: Chocolate\n", check[i]);
-					flag[j] = 1;
-				}
-				f = 0;
-				break;
-			}
-		}
-		if (f) printf("%s: Are you kidding?\n", check[i]);
+        int x = search(check[i], id, 0, N);
+
+        if (x == -1) printf("%s: Are you kidding?\n", check[i]);
+        else if (flag[x]) {
+            printf("%s: Checked\n", check[i]);
+            flag[x] = 1;
+        } else if (!x) {
+            printf("%s: Mystery Award\n", check[i]);
+            flag[x] = 1;
+        } else if (isPrime(x + 1)) {
+            printf("%s: Minion\n", check[i]);
+            flag[x] = 1;
+        } else {
+            printf("%s: Chocolate\n", check[i]);
+            flag[x] = 1;
+        }
     }
 
     return 0;

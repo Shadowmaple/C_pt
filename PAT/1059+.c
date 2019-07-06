942f81bc55898f7adc75e84f03a5e85c79bbe2cf
Shadowmaple <1142319190@qq.com>
2019-07-06 10:56:12 +0800

    modify

--- PAT/1059+.c
+++ PAT/1059+.c
@@ -1,11 +1,8 @@
 # include <stdio.h>
-# include <string.h>
-# include <math.h>
-# include <stdlib.h>
 
 int isPrime(int num)
 {
-    for (int i = 2; i < sqrt(num); i++)
+    for (int i = 2; i * i <= num; i++)
         if (num % i == 0)
             return 0;
     return 1;

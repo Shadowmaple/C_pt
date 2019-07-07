8be77e12203ca3970532f53c0c0d31f8b4bc96dc
Shadowmaple <1142319190@qq.com>
2019-07-07 09:16:18 +0800

    modify

--- PAT/1062.c
+++ PAT/1062.c
@@ -6,10 +6,12 @@ int main()
     scanf("%d/%d %d/%d %d", &a1, &b1, &a2, &b2, &K);
     double a = (double) a1 / b1;
     double b = (double) a2 / b2;
+
+    // 一个测试点，并没有指定谁大谁小
+    if (a > b) {double c = a; a = b; b = c;};
     
     int i = 1, flag = 0;;
-    while ((double) i / K <= a)
-        i++;
+    while ((double) i / K <= a) i++;
 
     for (; (double) i / K < b; i++) {
         int x = i, y = K;

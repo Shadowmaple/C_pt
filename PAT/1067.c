628298513293549efd5796cdaab8a34c23db6d2f
Shadowmaple <1142319190@qq.com>
2019-07-08 09:21:28 +0800

    modify

--- PAT/1067.c
+++ PAT/1067.c
@@ -6,17 +6,29 @@ int main()
     int n;
     char pw[21];
     scanf("%s%d", pw, &n);
+    getchar();
 
     char try[21];
     while (n--) {
-        scanf("%s", try);
+        // 重点
+        scanf("%20[^\n]", try);
+        char c = getchar();
 
         if (!strcmp(try, "#")) break;
-        else if (!strcmp(try, pw)) {
+        else if (!strcmp(try, pw) && c == '\n') {
             puts("Welcome in");
             break;
         } else {
-            printf("Wrong password: %s\n", try);
+            printf("Wrong password: %s", try);
+            if (c != '\n') {
+                printf("%c", c);
+                while (1) {
+                    scanf("%c", &c);
+                    if (c == '\n') break;
+                    printf("%c", c);
+                }
+            }
+            putchar('\n');
             if (!n)     puts("Account locked");
         }
     }

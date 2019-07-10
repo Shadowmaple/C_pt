05826444fcfb3ff58647c36eda7a22c02601ce1e
Shadowmaple <1142319190@qq.com>
2019-07-10 23:40:59 +0800

    modify

--- PAT/1070.c
+++ PAT/1070.c
@@ -1,10 +1,9 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <math.h>
 
 int compar(const void *a, const void *b)
 {
-    return *(int *) a < *(int *) b;
+    return *(int *) a > *(int *) b;
 }
 
 int main()
@@ -17,14 +16,11 @@ int main()
 
     qsort(num, n, sizeof(int), compar);
 
-    int max = num[0] / 2 + num[1] / 2;
-    for (int i = 2; i < n; i++) {
-        int len = max / 2 + num[i] / 2;
-        if (max > len) break;
-        max = len;
-    }
+    int len = num[0] / 2 + num[1] / 2;
+    for (int i = 2; i < n; i++)
+        len = (len + num[i]) / 2;
 
-    printf("%d\n", max);
+    printf("%d\n", len);
 
     return 0;
 }

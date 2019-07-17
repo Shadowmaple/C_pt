46a639aa107d0f8f4889f790e6298d071c89044f
Shadowmaple <1142319190@qq.com>
2019-07-17 12:58:22 +0800

    modify

--- PAT/1084.c
+++ PAT/1084.c
@@ -7,14 +7,14 @@ int main()
     scanf("%d %d", &d, &n);
 
     char result[100000], str[100000];
+    // 最后一个测试点很大
     result[0] = d + '0';
     result[1] = '\0';
   
     for (int j = 1; j < n; j++) {
-        int count = 1;
         char pre = result[0];
-        int index = 0;
-        int i = 1;
+        int count = 1, index = 0, i = 1;
+        int len = strlen(result);
         do {
             if (result[i] == pre) {
                 count++;
@@ -24,11 +24,11 @@ int main()
             str[index++] = count + '0';
             pre = result[i];
             count = 1;
-        } while (i++ < strlen(result));
+        } while (i++ < len);
+        // 这里不能用i++ < strlen(result)，会超时
 
         str[index] = '\0';
         strcpy(result, str);
-        // printf("%s\n", result);
     }
     printf("%s\n", result);
     return 0;

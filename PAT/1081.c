79daa1c66c729a35a8f43d7162d60ff341273c29
Shadowmaple <1142319190@qq.com>
2019-07-17 09:26:04 +0800

    new

---
+++ PAT/1081.c
@@ -0,0 +1,38 @@
+# include <stdio.h>
+# include <string.h>
+# include <ctype.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char psw[81];
+    for (int i = 0; i < n; i++) {
+        scanf("%s", psw);
+        int len = strlen(psw);
+        if (len < 6) {
+            puts("Your password is tai duan le.");
+            continue;
+        }
+        int haveDigit = 0, haveAlpha = 0;
+        int flag = 0;
+        for (int j = 0; j < len; j++) {
+            if (psw[j] != '.' && !isalnum(psw[j])) {
+                puts("Your password is tai luan le.");
+                flag = 1;
+                break;
+            }
+            if (isdigit(psw[j])) haveDigit = 1;
+            if (isalpha(psw[j])) haveAlpha = 1;
+        }
+        if (flag) continue;
+        
+        if (haveAlpha && haveDigit)
+            puts("Your password is wan mei.");
+        else if (haveAlpha)
+            puts("Your password needs shu zi.");
+        else
+            puts("Your password needs zi mu.");
+    }
+    return 0;
+}

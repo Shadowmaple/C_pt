2bd9959c36bb46a60daf393597254bbb60c06446
Shadowmaple <1142319190@qq.com>
2019-07-18 10:47:06 +0800

    modify

--- PAT/1086.c
+++ PAT/1086.c
@@ -5,10 +5,12 @@ int main()
     int a, b;
     scanf("%d %d", &a, &b);
     int multipy = a * b;
+    
+    while (!(multipy % 10))
+        multipy /= 10;
+
     while (multipy) {
-        int x = multipy % 10;
-        if (x)
-            printf("%d", x);
+        printf("%d", multipy % 10);
         multipy /= 10;
     }
     putchar('\n');
